36a7f64793d9afa6b90a520922c7b3485064eef1
Matt Oates <matt@boardom.io>
2020-03-31 11:13:18 -0700

    fixed?

--- __cdr.fish
+++ __cdr.fish
@@ -1,4 +1,4 @@
-function __cdgitroot_usage
+function __cdr_usage
     echo 'Usage: cdr [PATH]'
     echo 'Jump to git repository\'s root directory.'
     echo 'If PATH is specified, it\'s recognized as relative path'
@@ -6,7 +6,7 @@ function __cdgitroot_usage
 end
 
 function cdr -d 'Jump to git repo\'s root'
-    if not __cdgitroot_in_repo
+    if not __cdr_in_repo
         echo 'It\'s out of working tree!' 1>&2
         return 1
     end
@@ -20,20 +20,20 @@ function cdr -d 'Jump to git repo\'s root'
         set relative_path $argv
         cd "$root_path/$relative_path"
     else
-        __cdgitroot_usage
+        __cdr_usage
     end
 end
 
-function __cdgitroot_complete
+function __cdr_complete
     set root_path (git rev-parse --show-toplevel)
     command find $root_path -type d -name '.git' -prune -o -type d -print \
             | sed "s#$root_path/##g" | sed "s#$root_path##g"
 end
 
-function __cdgitroot_in_repo
+function __cdr_in_repo
     command git rev-parse --git-dir > /dev/null ^ /dev/null
     return $status
 end
 
-complete -c cdr --no-files -d 'path' --condition __cdgitroot_in_repo -a '(__cdgitroot_complete)'
+complete -c cdr --no-files -d 'path' --condition __cdr_in_repo -a '(__cdr_complete)'
 
